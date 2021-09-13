6c23576247882150a599a59d6e360f421f0d250d
Daphny Barbosa <daphnyemily@Daphnys-MacBook-Pro.local>
2021-09-13 13:16:22 -0400

    adding functionHelloWorld

---
+++ 8kyu/functionHelloWorld.js
@@ -0,0 +1,9 @@
+
+//https://www.codewars.com/kata/523b4ff7adca849afe000035/solutions/javascript
+
+
+// Write a function "greet" that returns "hello world!"
+
+function greet() {
+    return("hello world!")
+  }
