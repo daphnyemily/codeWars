3c2012c15e19dd4f6f4913045d73ff0f4857b7e7
daphnyemily <daphnyemily@daphnys-mbp.lan>
2021-09-14 21:01:34 -0400

    adding new codewar solution

---
+++ 8kyu/jennyGreetJohnny.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/javascript
+
+function greet(name){
+    if(name === "Johnny")
+      return "Hello, my love!"
+      return "Hello, " + name + "!"
+  }
