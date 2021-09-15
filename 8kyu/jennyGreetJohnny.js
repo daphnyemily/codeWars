d343c33f947c4ec84cc95e2a15695800827dc833
daphnyemily <daphnyemily@daphnys-mbp.lan>
2021-09-14 21:15:20 -0400

    adding jennyGreetJohnny

--- 8kyu/jennyGreetJohnny.js
+++ 8kyu/jennyGreetJohnny.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/javascript//
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/javascript
 
 function greet(name){
     if(name === "Johnny")
