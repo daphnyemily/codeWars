27ed33faa41541b677bc23c49de0e647a5d2322f
daphnyemily <daphnyemily@daphnys-mbp.lan>
2021-09-22 20:00:47 -0400

    adding return negative solution

---
+++ 8kyu/returnNegative.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+function makeNegative(num) {
+    if (num>0){
+      return -num
+    }
+    else if (num<0){
+      return num
+    }
+    else {
+      return 0
+    }
+  }
