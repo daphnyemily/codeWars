6479e944ff4753f7b2e66d2e65a0b1386e98e1e8
daphnyemily <daphnyemily@Daphnys-MacBook-Pro.local>
2021-09-28 12:01:39 -0400

    adding counting sheep solution

---
+++ 8kyu/countingSheep.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/54edbc7200b811e956000556
+
+
+function countSheeps(arrayOfSheeps){
+    let num=0;
+  
+    for(let i=0; i<arrayOfSheeps.length; i++){  
+      
+    if(arrayOfSheeps[i]==true){ 
+        num++; 
+    }
+  } 
+      return num;
+}
