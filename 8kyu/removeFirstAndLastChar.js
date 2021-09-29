93e0d7ddcf00a7583af15960cdf42936ac45a11b
Daphny Barbosa <daphnyemily@gmail.com>
2021-09-29 18:02:28 -0400

    Update removeFirstAndLastChar.js

--- 8kyu/removeFirstAndLastChar.js
+++ 8kyu/removeFirstAndLastChar.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/solutions/javascript
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/solutions/javascript //
 
 function removeChar(str){
     return str.slice(1, str.length -1)
