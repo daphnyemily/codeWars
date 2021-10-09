bfbe6660215430d284f3ff743458889469a384c9
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-09 19:30:30 -0400

    Update DisemvowelTrolls.js

--- 8kyu/DisemvowelTrolls.js
+++ 8kyu/DisemvowelTrolls.js
@@ -1,4 +1,4 @@
-//https://www.codewars.com/kata/52fba66badcd10859f00097e//
+//https://www.codewars.com/kata/52fba66badcd10859f00097e
 
 function disemvowel(str) {
     return str.replace (/[aeiou]/gi, '');
