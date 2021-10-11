96c4b4987809fc77d7218b2b7af4f1cf9793cf5f
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-10 22:26:40 -0400

    adding remove string space

---
+++ 8kyu/removeStringSpace.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript
+
+function noSpace(x){
+    return x.replace(/\s+/g,'')
+  }
+  
+  // \s = any white space
+  //g = global match
