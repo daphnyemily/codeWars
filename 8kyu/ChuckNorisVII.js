23643e5fb57d9a652d132f2e843902ad8179bb58
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-22 00:23:43 -0400

    adding Chuck Noris VII solution

--- 8kyu/ChuckNorisVII.js
+++
@@ -1,8 +0,0 @@
-//https://www.codewars.com/kata/570669d8cb7293a2d1001473/solutions/javascript
-
-function ifChuckSaysSo(theTruth){
-    if (theTruth == true){
-      return theTruth
-    }else
-      return theTruth!= theTruth
-}
