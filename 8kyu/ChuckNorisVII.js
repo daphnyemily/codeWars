fcf667de5cda1562774f784a1cdb2b4d80b8c4e7
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-26 19:39:04 -0400

    adding Messi Goals solution

---
+++ 8kyu/ChuckNorisVII.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/570669d8cb7293a2d1001473/solutions/javascript
+
+function ifChuckSaysSo(theTruth){
+    if (theTruth == true){
+      return theTruth
+    }else
+      return theTruth!= theTruth
+}
