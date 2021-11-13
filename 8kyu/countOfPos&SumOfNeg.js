56edd56277d657f2b526b401fa744c8cbffe9adf
Daphny Barbosa <daphnyemily@gmail.com>
2021-11-12 22:33:13 -0500

    adding remove first and last char part 2 solution

--- 8kyu/countOfPos&SumOfNeg.js
+++ 8kyu/countOfPos&SumOfNeg.js
@@ -11,3 +11,27 @@ function countPositivesSumNegatives(input) {
        let greaterThan = input.filter(a => a>0)
        return [greaterThan.length, lessThan.reduce(reducer, 0)]
     }}
+
+     
+
+        //input or an array
+        //input can be positive or negative
+        //no strings (all nums)
+        //array can empty or null
+
+        // Returning an array with two numbers
+        // One number will be the count total of every postiive number
+        // Other number will be the sum total of the negative numbers
+        
+ 
+        //[1,2,3,4,5, 6, 7, 8, 9, 10-11,-12,-13,-14,-15] => [10, -65]
+        // Example 2
+        // Example 3
+        
+  
+        // if statement covering those conditions empty or null
+        // return empty array
+        // reducer = (a,b) => return a+b
+        // filter both a < 0
+        // filter a > 0
+        //return a < 0 , a > 0
