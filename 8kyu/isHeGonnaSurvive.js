f83a911a78bf1eddfbaec27ca680195ee25fe8a2
Daphny Barbosa <daphnyemily@gmail.com>
2021-11-18 21:09:44 -0500

    adding is he gonna survive solution

---
+++ 8kyu/isHeGonnaSurvive.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons){
+    if(bullets >= dragons*2){
+      return true
+    }else {
+      return false
+    }
+   }
