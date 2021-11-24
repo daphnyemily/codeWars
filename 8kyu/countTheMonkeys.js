db9f8796e32c30d9e827db0eab0272c78bba5445
Daphny Barbosa <daphnyemily@gmail.com>
2021-11-23 21:27:05 -0500

    adding count the monkeys solution

---
+++ 8kyu/countTheMonkeys.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+
+function monkeyCount(n) {
+    let monkey = []
+    
+    for(let i = 1; i <= n; i++){
+      monkey.push(i)
+    }
+    return monkey
+  }
